5013447126913c2c0329827560bbca6ed31143e2
Robin Werth <rbwr3951@umwelt-campus.de>
2026-02-11 13:04:12 +0100

    chore: set up CLI library and change CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
-//
-// Created by robin on 11.02.2026.
-//
+#include <CLI/CLI.hpp>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    CLI::App app{"ImageProcessing CLI"};
+
+    int value = 0;
+    app.add_option("-v,--value", value, "Ein Beispielwert");
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "Verarbeiteter Wert: " << value << std::endl;
+
+    return 0;
+}
